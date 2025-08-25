59ddd00aab9c437d2f4e62a16146f3685df28519
rizqyn9 <rizqynugroho9@gmail.com>
2025-08-25 22:14:14 +0700

    feat: onesignal

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
 import 'package:scaled_app/scaled_app.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -16,7 +17,39 @@ import 'shared/utils/talker.dart';
 // flutter: padding: EdgeInsets(0.0, 24.0, 0.0, 25.0)
 //
 
+const double widthOfDesign = 1180;
+
 void main() async {
+  ScaledWidgetsFlutterBinding.ensureInitialized(scaleFactor: (deviceSize) => deviceSize.width / widthOfDesign);
+
+  await setPreferredOrientations();
+  await initializeDateFormatting('id_ID');
+
+  final db = await initSembastDb();
+  final sp = await SharedPreferences.getInstance();
+
+  runApp(
+    ProviderScope(
+      observers: const [TalkerStateLogger()],
+      overrides: [
+        sembastServiceProvider.overrideWithValue(SembastService(db: db)),
+        sharedPrefsProvider.overrideWithValue(sp),
+      ],
+      child: const IkkiPosApp(),
+    ),
+  );
+}
+
+Future<void> initOneSignal() async {
+  // Enable verbose logging for debugging (remove in production)
+  await OneSignal.Debug.setLogLevel(OSLogLevel.verbose);
+  OneSignal.initialize('YOUR_APP_ID');
+  // Use this method to prompt for push notifications.
+  // We recommend removing this method after testing and instead use In-App Messages to prompt for notification permission.
+  await OneSignal.Notifications.requestPermission(false);
+}
+
+Future<void> initNewRelic() async {
   // var appToken = '';
   // if (Platform.isIOS) {
   //   appToken = 'AAf4e0b2a3b17f3a07f6b173e6434adf60487742e4-NRMA';
@@ -28,38 +61,8 @@ void main() async {
   //   accessToken: appToken,
   //   newEventSystemEnabled: true,
   // );
-
-  ScaledWidgetsFlutterBinding.ensureInitialized(
-    scaleFactor: (deviceSize) {
-      // screen width used in your UI design
-      const double widthOfDesign = 1180;
-      // const double widthOfDesign = 460;
-      return deviceSize.width / widthOfDesign;
-    },
-  );
-
-  await setPreferredOrientations();
-
-  final db = await initSembastDb();
-  final sp = await SharedPreferences.getInstance();
-
   // await NewrelicMobile.instance.start(config, () {});
   // await NewrelicMobile.instance.setMaxEventPoolSize(3000);
   // await NewrelicMobile.instance.setMaxEventBufferTime(200);
   // await NewrelicMobile.instance.setMaxOfflineStorageSize(200);
-
-  await initializeDateFormatting(
-    'id_ID',
-  ).then((_) {
-    runApp(
-      ProviderScope(
-        observers: const [TalkerStateLogger()],
-        overrides: [
-          sembastServiceProvider.overrideWithValue(SembastService(db: db)),
-          sharedPrefsProvider.overrideWithValue(sp),
-        ],
-        child: const IkkiPosApp(),
-      ),
-    );
-  });
 }
